a5f9ea8b27fc3a65db950c0e6a8766e96b9ef061
Extreme-S <1716224950@qq.com>
2023-12-19 21:26:11 +0800

    StringQuery 与 NativeQuery 查询实践

--- src/test/java/com/example/esstudy/EsProjectApplicationTests.java
+++ src/test/java/com/example/esstudy/EsProjectApplicationTests.java
@@ -1,21 +1,29 @@
 package com.example.esstudy;
 
+import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
+import co.elastic.clients.elasticsearch._types.aggregations.Buckets;
+import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
 import com.example.esstudy.model.VideoDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchAggregation;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchAggregations;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.StringQuery;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @SpringBootTest
@@ -183,6 +191,50 @@ class XdclassEsProjectApplicationTests {
             list.add(hit.getContent());
         });
         System.out.println(list);
+    }
+
+    @Test
+    void stringQuery() {
+        //搜索标题有 架构 关键词，描述有 spring关键字，时长范围是 10～6000之间的
+        String dsl = """
+                   {"bool":{"must":[{"match":{"title":"架构"}},{"match":{"description":"spring"}},{"range":{"duration":{"gte":10,"lte":6000}}}]}}
+                """;
+        List<SearchHit<VideoDTO>> searchHitList = restTemplate.search(new StringQuery(dsl), VideoDTO.class).getSearchHits();
+
+        // 获得searchHits,进行遍历得到content
+        List<VideoDTO> videoDTOS = searchHitList.stream().map(SearchHit::getContent).toList();
+        videoDTOS.forEach(System.out::println);
+    }
+
+    @Test
+    void aggQuery() {
+
+        NativeQuery query = NativeQuery.builder()
+                .withAggregation("category_group", Aggregation.of(a -> a.terms(ta -> ta.field("category").size(10))))
+                .build();
+        System.out.println(query.getQuery());
+//
+//        String dsl = """
+//
+//                """
+
+        SearchHits<VideoDTO> search = restTemplate.search(query, VideoDTO.class);
+
+        ElasticsearchAggregations aggregations = (ElasticsearchAggregations) search.getAggregations();
+
+        Map<String, ElasticsearchAggregation> aggregationMap = aggregations.aggregationsAsMap();
+
+        ElasticsearchAggregation categoryGroup = aggregationMap.get("category_group");
+
+        Buckets<StringTermsBucket> buckets = categoryGroup.aggregation().getAggregate().sterms().buckets();
+
+        buckets.array().forEach(bucket -> {
+            System.out.println("组名：" + bucket.key().stringValue() + ",value=" + bucket.docCount());
+        });
+
+        // 获得searchHits,进行遍历得到content
+        List<VideoDTO> videoDTOS = search.stream().map(SearchHit::getContent).toList();
+        videoDTOS.forEach(System.out::println);
 
     }
 }
